906faf80ef670a5b004d76050458b6b5393447a1
Luiz Gustavo Marcondes <luizgustavodevergennes@gmail.com>
2023-05-02 10:27:44 -0300

    fix: Ajustado tela inicial como Login

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Messages">
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={SlideInHome} 
