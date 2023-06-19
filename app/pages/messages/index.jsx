a72db0852c9290b1eb18301ca250eea8e86edb7d
Luiz Gustavo Marcondes <luizgustavodevergennes@gmail.com>
2023-06-19 02:12:19 +0000

    :sparkles: API

--- app/pages/messages/index.jsx
+++ app/pages/messages/index.jsx
@@ -3,21 +3,58 @@ import Header from "../../components/Header/HeaderMessages";
 import Chats from "../../components/Chats/index";
 import Menu from "../../components/Menu/Menu";
 import AddButton from "../../components/Buttons/AddButton";
+import api from "../../api";
+import { useAuth } from "../../contexts/auth";
+import { useEffect, useState } from "react";
 
 const Messages = ({navigation}) => {  
+    const { signOut } = useAuth()
+    const [chats, setChats] = useState([]);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        api.getMessages()
+            .then(res => {
+                switch (res.status) {
+                    case 401:
+                        signOut();
+                    case 200:
+                        setChats(res.data);
+                        break;
+                    default:
+                        throw new Error(res.data);
+                }
+            })
+            .catch(err => {
+                setIsError(true);
+                if ( err instanceof Error) {
+                    setError(err.message);
+                } else {
+                    setError("Ocorreu um erro ao carregar as mensagens.");
+                }
+            })
+            .finally(()=> {
+                setIsLoading(false);
+            })
+    }, []);
+
     return (
         <View style={styles.Container}>
             <Header navigation={navigation}/>
             <ScrollView contentContainerStyle={{ rowGap: '24px', marginBottom: '4rem'}} style={styles.Messages}>
                 <View>
-                    <Chats />
-                    <Chats />
-                    <Chats />
+                {
+                    isLoading ? <Text>Carregando...</Text> :
+                    isError ? <Text>{error}</Text> :
+                    chats ? chats.map(chat=> <Chats chat={chat}/>) : (<Text>Você não possui conversas no momento.</Text>)
+                }
                 </View>
                 <View style={{alignItems:'center', top:'75px'}}>
-                    <Text style={{color:"#d8d8d8", fontWeight:400, marginBottom:'20px'}}>Você não possui mais conversas no momento.</Text>
                     <TouchableOpacity>
-                    <Text style={{color:"#fff", fontWeight:700}}>Iniciar nova conversa</Text>
+                        <Text style={{color:"#fff", fontWeight:700}}>Iniciar nova conversa</Text>
                     </TouchableOpacity>
                 </View>
             </ScrollView>      
