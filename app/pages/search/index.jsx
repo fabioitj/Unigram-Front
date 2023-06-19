d0116193e080ca74609dffe42335cccfa7d154f7
Luiz Gustavo Marcondes <luizgustavodevergennes@gmail.com>
2023-06-19 02:12:30 +0000

    :sparkles: API

--- app/pages/search/index.jsx
+++ app/pages/search/index.jsx
@@ -2,15 +2,59 @@ import { StyleSheet, View, Image, TouchableOpacity, Text, ScrollView } from "rea
 import Header from '../../components/Header/HeaderSearch';
 import Menu from "../../components/Menu/Menu";
 import SearchList from "../../components/SearchList";
+import { useEffect, useState } from "react";
+import api from "../../api";
 
 const Search = ({navigation}) => {
+    const { signOut } = useAuth()
+    const [search, setSearch] = useState();
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        api.findUser(search)
+            .then(res => {
+                switch (res.status) {
+                    case 401:
+                        signOut();
+                    case 200:
+                        setUsers(res.data);
+                        break;
+                    default:
+                        throw new Error(res.data);
+                }
+            })
+            .catch(err => {
+                setIsError(true);
+                if ( err instanceof Error) {
+                    setError(err.message);
+                } else {
+                    setError("Ocorreu um erro ao buscar os usuários.");
+                }
+            })
+            .finally(()=> {
+                setIsLoading(false);
+            })
+    }, [search]);
     return(
 
         <View style={styles.Container}>
         <Header navigation={navigation}/>
         <ScrollView contentContainerStyle={{ rowGap: '24px', marginBottom: '4rem'}} style={styles.Searches}>
             <View>
-                <SearchList/>
+                <TextInput
+                    placeholder="Nome do usuário"
+                    value={imageLink}
+                    onChangeText={text => setSearch(text)}
+                />
+                {
+                    isLoading ? <Text>Carregando...</Text> :
+                    isError ? <Text>{error}</Text> :
+                    users && users.map(user=><SearchList post={user}/>)
+                }
             </View>
             <View style={{alignItems:'center', top:'75px'}}>
                 <Text style={{color:"#fff", fontWeight:600, marginBottom:'20px', textAlign:'center'}}>Não há mais nenhum usuário com{'\n'} este nome ou similar.</Text>
